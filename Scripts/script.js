92d90dd3916722e594b78b3d771c52e82a67c627
themusicdude <ds8uy09@limestone.on.ca>
2026-09-11 14:05:13 -0400

    Fix textContainer assignment to reference element

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,6 +2,6 @@
 fetch('dates.txt') 
   .then(response => response.text()) 
   .then(data => { 
-    const textContainer = document.getElementById('date-text').textContent;
+    const textContainer = document.getElementById('date-text');
     textContainer.textContent = data;
   }); 
