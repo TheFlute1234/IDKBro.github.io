301c351c154c8231fa31a5f93b91f8c71e2792dd
themusicdude <ds8uy09@limestone.on.ca>
2026-09-14 14:03:04 -0400

    Update fetch calls to load event dates from new files

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,34 @@
-// Change 'document.txt' to match your actual filename 
-fetch('dates.txt') 
+fetch('../event_dates/blue_band.txt') 
   .then(response => response.text()) 
   .then(data => { 
-    const textContainer = document.getElementById('date-text');
+    const textContainer = document.getElementById('blue-text');
+    textContainer.textContent = data;
+  }); 
+
+fetch('../event_dates/gold_band.txt') 
+  .then(response => response.text()) 
+  .then(data => { 
+    const textContainer = document.getElementById('gold-text');
+    textContainer.textContent = data;
+  });
+
+fetch('../event_dates/stage_band.txt') 
+  .then(response => response.text()) 
+  .then(data => { 
+    const textContainer = document.getElementById('stage-text');
+    textContainer.textContent = data;
+  }); 
+
+fetch('../event_dates/class.txt') 
+  .then(response => response.text()) 
+  .then(data => { 
+    const textContainer = document.getElementById('class-text');
+    textContainer.textContent = data;
+  }); 
+
+fetch('../event_dates/drama.txt') 
+  .then(response => response.text()) 
+  .then(data => { 
+    const textContainer = document.getElementById('drama-text');
     textContainer.textContent = data;
   }); 
