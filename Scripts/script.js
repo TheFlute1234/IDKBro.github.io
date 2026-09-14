a0eed916ed65e7a1bf26d1f18135d4ddf833082f
themusicdude <ds8uy09@limestone.on.ca>
2026-09-14 14:12:38 -0400

    Update fetch paths to use relative URLs

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,32 +1,32 @@
-fetch('../event_dates/blue_band.txt') 
+fetch('./event_dates/blue_band.txt') 
   .then(response => response.text()) 
   .then(data => { 
     const textContainer = document.getElementById('blue-text');
     textContainer.textContent = data;
   }); 
 
-fetch('../event_dates/gold_band.txt') 
+fetch('./event_dates/gold_band.txt') 
   .then(response => response.text()) 
   .then(data => { 
     const textContainer = document.getElementById('gold-text');
     textContainer.textContent = data;
   });
 
-fetch('../event_dates/stage_band.txt') 
+fetch('./event_dates/stage_band.txt') 
   .then(response => response.text()) 
   .then(data => { 
     const textContainer = document.getElementById('stage-text');
     textContainer.textContent = data;
   }); 
 
-fetch('../event_dates/class.txt') 
+fetch('./event_dates/class.txt') 
   .then(response => response.text()) 
   .then(data => { 
     const textContainer = document.getElementById('class-text');
     textContainer.textContent = data;
   }); 
 
-fetch('../event_dates/drama.txt') 
+fetch('./event_dates/drama.txt') 
   .then(response => response.text()) 
   .then(data => { 
     const textContainer = document.getElementById('drama-text');
