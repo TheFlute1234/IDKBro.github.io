3559f83e25adbbde4ed0f258d00b04c2edd33f01
themusicdude <ds8uy09@limestone.on.ca>
2026-09-15 14:09:34 -0400

    Remove unused fetch calls for event dates
    Removed fetch calls for blue_band, stage_band, class, and drama event dates.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,37 +1,9 @@
-fetch('./event_dates/blue_band.txt') 
-  .then(response => response.text()) 
-  .then(data => { 
-    const textContainer = document.getElementById('blue-text');
-    textContainer.textContent = data;
-  }); 
-
 fetch('./event_dates/gold_band.txt') 
   .then(response => response.text()) 
   .then(data => { 
     const textContainer = document.getElementById('gold-text');
     textContainer.textContent = data;
   });
-
-fetch('./event_dates/stage_band.txt') 
-  .then(response => response.text()) 
-  .then(data => { 
-    const textContainer = document.getElementById('stage-text');
-    textContainer.textContent = data;
-  }); 
-
-fetch('./event_dates/class.txt') 
-  .then(response => response.text()) 
-  .then(data => { 
-    const textContainer = document.getElementById('class-text');
-    textContainer.textContent = data;
-  }); 
-
-fetch('./event_dates/drama.txt') 
-  .then(response => response.text()) 
-  .then(data => { 
-    const textContainer = document.getElementById('drama-text');
-    textContainer.textContent = data;
-  }); 
 
 const calendarDates = document.querySelector('.calendar-dates');
 const monthYear = document.getElementById('month-year');
